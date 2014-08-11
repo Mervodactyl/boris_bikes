886dfdb50138ec76293db40e5efadd12fff5b42c
Mervodactyl <merve.silk@gmaillcom>
2014-08-11 16:49:21 +0100

    secound commit

---
+++ lib/bike.rb
@@ -0,0 +1,12 @@
+class bike
+	def intialize 
+		@broken = false
+	end
+	def broken?
+	end
+
+
+	def break!
+		@broken = true
+	end 
+end 
---
+++ spec/bike_spec.rb
@@ -0,0 +1,14 @@
+require 'bike'
+
+# we're describing the funcunality of a specific class, Bike
+describe Bike do 
+	# this a specific feature (behaiviour)
+	# that we expect to be present
+	it "should not be broken after we create it" do
+		the_bike = Bike.new # intialise a new object of Bike class
+		# expect an instance of this class to have
+		# a method "broken? that should be false
+		bike.break!
+		expect(the_bike) .not be_broken
+	end
+end
