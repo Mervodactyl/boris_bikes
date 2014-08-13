1680b0a9323521ea489f12c75e055128d03fe874
David Wickes and Mervé Silk <merve.silk+dave+merve@gmail.com>
2014-08-13 17:44:57 +0100

    new test for van, should have no bikes to begin with

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -1,7 +1,7 @@
 require './lib/van.rb'
 
 describe 'a van' do
-  it 'should hhave no bikes to begin with' do
+  it 'should have no bikes to begin with' do
     expect(Van.new.bike_count).to eq(0)
   end
 end
